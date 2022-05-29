1508c26341a508ff737db5ac48933f20cb57ec2a
tofudoctor <87269360+tofudoctor@users.noreply.github.com>
2022-05-30 00:16:12 +0800

    change student teacher staff

--- Staff.java
+++ Staff.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
 public class Staff extends Member{
+    private int count;
     public Staff(String name, String password){
         super(name, password);
+    }
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void borrow_book(Book b){
+        if(count<=30){
+            books.add(b);
+            count++;
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "³Ì¦h­É10¥»!!!");
+        }
+    }
+    public void return_book(Book b){
+        books.remove(b);
+        count--;
     }
 }
--- Teacher.java
+++ Teacher.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
 public class Teacher extends Member{
+    private int count;
     public Teacher(String name, String password){
         super(name, password);
+    }
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void borrow_book(Book b){
+        if(count<=20){
+            books.add(b);
+            count++;
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "³Ì¦h­É10¥»!!!");
+        }
+    }
+    public void return_book(Book b){
+        books.remove(b);
+        count--;
     }
 }
