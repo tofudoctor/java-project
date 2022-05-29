bb1e41de948541b6b82e0471a7b85411e7d9fde3
tofudoctor <87269360+tofudoctor@users.noreply.github.com>
2022-05-30 00:13:25 +0800

    change sth

--- Student.java
+++ Student.java
@@ -1,8 +1,9 @@
+import javax.swing.*;
 public class Student extends Member{
     private int count;
-    public Student(String name, String password, int count){
+    public Student(String name, String password){
         super(name, password);
-        setCount(count);
+        setCount(0);
     }
     
     public int getCount() {
@@ -14,9 +15,16 @@ public class Student extends Member{
     }
 
     public void borrow_book(Book b){
-        books.add(b);
+        if(count<=10){
+            books.add(b);
+            count++;
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "³Ì¦h­É10¥»!!!");
+        }
     }
     public void return_book(Book b){
         books.remove(b);
+        count--;
     }
 }
